2a47b0b55264f398eff4fa2f261948f7ef266dcb
Veronika Tuzhyk <veronikatuzik@gmail.com>
2025-02-24 14:05:07 +0100

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,39 @@
 `use scrict`
 
-function formatMessage(message, maxLength) {
-  // Перевірка довжини рядка
-  if (message.length <= maxLength) {
-      return message;
-  }
+function checkForSpam(message) {
+  // Приводимо рядок до нижнього регістру для коректного порівняння
+  const lowerCaseMessage = message.toLowerCase();
   
-  // Обрізка рядка та додавання "..."
-  return message.slice(0, maxLength) + "...";
+  // Перевіряємо, чи містить рядок заборонені слова
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
 
-// Перевірка коректності функції
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+// Перевірка функції checkForSpam
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
